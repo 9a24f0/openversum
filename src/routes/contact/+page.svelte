b67cc7d6b8942b8f722cd4155013368676588e62
tuancan <tuacandongsang@gmail.com>
2023-02-14 23:34:06 +0700

    Rebase from redesign branch

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -8,26 +8,28 @@
 	let currentForm = 'Contact';
 </script>
 
-<div class="contact_container ">
+<div>
 	{#if currentForm === 'Contact'}
 		<div class="h-[calc(100vh-13rem)] bg-blue-500 w-full flex justify-center items-center">
 			<ContactMain bind:currentFormChild={currentForm} />
 		</div>
 	{:else }
-		<div class="flex">
-			<ContactNavigation/>
-			{#if currentForm === 'general'}
-				<ContactDefault/>
-			{/if}
-			{#if currentForm === 'entrepreneur'}
-				<ContactEntrepreneur/>
-			{/if}
-			{#if currentForm === 'partner'}
-				<ContactPartner/>
-			{/if}
+		<div class="flex items-center h-[calc(100vh-13rem)] justify-evenly py-24">
+			<div class="pl-32 flex-1">
+				<ContactNavigation bind:currentFormChild={currentForm}/>
+			</div>
+
+			<div class="pr-28 w-3/5 ">
+				{#if currentForm === 'general'}
+					<ContactDefault/>
+				{/if}
+				{#if currentForm === 'entrepreneur'}
+					<ContactEntrepreneur/>
+				{/if}
+				{#if currentForm === 'partner'}
+					<ContactPartner/>
+				{/if}
+			</div>
 		</div>
 	{/if}
 </div>
-
-<style>
-</style>
