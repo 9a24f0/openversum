d45c4b144ad0475362c79800dbef58c9d48d071a
tuancan <tuacandongsang@gmail.com>
2023-02-14 21:18:50 +0700

    minh xem truoc cho a doan nay nhe

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,17 +1,44 @@
 <script>
 	import ContactMain from '$lib/components/contact/ContactMain.svelte';
-	// import ContactDefault from '$lib/components/contact/ContactDefault.svelte';
-	// import ContactEntrepreneur from '$lib/components/contact/ContactEntrepreneur.svelte';
-	// import ContactPartner from '$lib/components/contact/ContactPartner.svelte';
+	import ContactDefault from '$lib/components/contact/ContactDefault.svelte';
+	import ContactEntrepreneur from '$lib/components/contact/ContactEntrepreneur.svelte';
+	import ContactPartner from '$lib/components/contact/ContactPartner.svelte';
 
-	// let currentForm = 'contact';
+	let currentForm = 'Contact';
 </script>
 
-<ContactMain />
-<!-- {#if currentForm === 'general'}
-	<ContactDefault />
-{:else if currentForm === 'entrepreneur'}
-	<ContactEntrepreneur />
-{:else if currentForm === 'partner'}
-	<ContactPartner />
-{/if} -->
+<div class="contact_container bg-blue-500 w-full flex justify-center items-center">
+	{#if currentForm === 'Contact'}
+		<ContactMain bind:currentForm1={currentForm} />
+	{:else}
+		<div class=" w-1/2 pl-32 ">
+			<div class="w-176 ">
+				<ContactMain bind:currentForm1={currentForm} />
+			</div>
+		</div>
+		<div class=" w-1/2 pr-52 ">
+			{#if currentForm === 'General'}
+				<ContactDefault />
+			{:else if currentForm === 'Emprendedor'}
+				<ContactEntrepreneur />
+			{:else if currentForm === 'Socio'}
+				<ContactPartner />
+			{/if}
+		</div>
+	{/if}
+	<!-- {#if currentForm === 'Contact'}
+		<ContactMain bind:currentForm1={currentForm} />
+	{:else if currentForm === 'General'}
+		<ContactDefault />
+	{:else if currentForm === 'Emprendedor'}
+		<ContactEntrepreneur />
+	{:else if currentForm === 'Socio'}
+		<ContactPartner />
+	{/if} -->
+</div>
+
+<style>
+	.contact_container {
+		height: calc(100vh - 8rem - 80px);
+	}
+</style>
