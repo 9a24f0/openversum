398d6c54e28cbfef659c3e4140bd96b8b28b28df
tuancan <tuacandongsang@gmail.com>
2023-02-14 20:11:38 +0700

    select contact

---
+++ src/lib/components/contact/ContactMain.svelte
@@ -0,0 +1,41 @@
+<script>
+	let currentForm = 'contact';
+	function changeStateContact(x) {
+		currentForm = x;
+		console.log(currentForm);
+	}
+</script>
+
+<div class="contact_container bg-blue-500 w-full flex justify-center items-center">
+	<ul class="font-medium not-italic semi-bold flex flex-col gap-6">
+		<li
+			class="contact_container-item self-center cursor-pointer"
+			on:click={() => changeStateContact('General')}
+		>
+			General
+		</li>
+		<li
+			class="contact_container-item self-center cursor-pointer"
+			on:click={() => changeStateContact('Emprendedor')}
+		>
+			Emprendedor
+		</li>
+		<li
+			class="contact_container-item self-center cursor-pointer"
+			on:click={() => changeStateContact('Socio')}
+		>
+			Socio
+		</li>
+	</ul>
+</div>
+
+<style>
+	.contact_container {
+		height: calc(100vh - 8rem - 80px);
+	}
+	.contact_container-item {
+		color: theme('colors.white.DEFAULT');
+		font-size: theme('fontSize.45');
+		font-family: theme('fontFamily.poppins');
+	}
+</style>
